071aee166832ecb0096400cbcaeee5feb4111c12
ohjy0324 <ohjy0324@naver.com>
2016-07-21 13:19:18 +0900

    1번

--- app/src/main/java/com/example/user/userapp/MainActivity.java
+++ app/src/main/java/com/example/user/userapp/MainActivity.java
@@ -1,30 +1,35 @@
 package com.example.user.userapp;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     Button btn;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        android.support.v7.app.ActionBar ab = getSupportActionBar();
-        ab.setTitle("LOGIN");
-        setContentView(R.layout.loginlayout);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.loginlayout);
 
-        btn =  (Button)findViewById(R.id.loginBtn);
+            btn =  (Button)findViewById(R.id.loginBtn);
 
-        btn.setOnClickListener(new View.OnClickListener() {
-            public  void  onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, PublicActivity.class);
+            btn.setOnClickListener(new View.OnClickListener() {
+                public  void  onClick(View view) {
+                    Intent intent = new Intent(MainActivity.this, PublicActivity.class);
 
-                startActivity(intent);
-            }
-        });
+                    startActivity(intent);
+                }
+            });
+
+            Toolbar toolbar = (Toolbar) findViewById(R.id.toolBar);
+            toolbar.setTitle("login");
+            toolbar.setTitleTextColor(Color.WHITE);
+            setSupportActionBar(toolbar);
 
     }
 }
