5b480857731a6e898200abf4efd9b588d596d70d
ohjy0324 <ohjy0324@naver.com>
2016-08-04 13:11:36 +0900

    2번

---
+++ app/src/main/java/com/example/user/userapp/onNetworkResponseListener.java
@@ -0,0 +1,12 @@
+package com.example.user.userapp;
+
+
+import org.json.JSONObject;
+
+/**
+ * Created by dilky on 2016-08-03.
+ */
+public interface onNetworkResponseListener {
+    void onSuccess(String api_key, JSONObject response);
+    void onFailure(String api_key, String error_cd, String error_msg);
+}
